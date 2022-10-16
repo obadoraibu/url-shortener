4dd9b5a25246e9512e586cf2d8b0b0740221299f
Egor Zavyalov <zes-amur@list.ru>
2022-10-16 12:49:00 +0300

    added logging

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,18 +1,38 @@
 package apiserver
 
-// APIserver
+import "github.com/sirupsen/logrus"
+
+// APIserver structure
 type APIserver struct {
 	config *Config
+	logger *logrus.Logger
 }
 
-// New
+// New server object
 func New(config *Config) *APIserver {
 	return &APIserver{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 // Start server
 func (s *APIserver) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting the server on ", s.config.BindAddr)
+	return nil
+}
+
+func (s *APIserver) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
 }
